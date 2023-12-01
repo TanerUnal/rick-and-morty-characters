70ac5f306848cfb6fdb7a1243de150acce1e8c96
Taner Ünal <m.taner.unal@gmail.com>
2023-12-01 12:39:44 +0100

    chore: initial project configurations

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+  declarations: [],
+  exports: [],
+  imports: [
+    RouterModule
+  ],
+  providers: []
+})
+export class CoreModule {
+}
